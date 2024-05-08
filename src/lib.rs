f9ccc2fa776701567c8bad6bcb1adbae81df0c2b
clement <clementwanjau@gmail.com>
2024-05-09 01:47:05 +0300

    Rename the `leptos-component` feature to `leptos`.

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ mod bindings;
 
 #[cfg(feature = "yew-component")]
 mod yew;
-#[cfg(feature = "leptos-component")]
+#[cfg(feature = "leptos")]
 mod leptos;
 
 pub mod prelude {
@@ -13,6 +13,6 @@ pub mod prelude {
 	pub use crate::options::{ChartType, ChartSeries, SeriesData, to_jsvalue};
 	#[cfg(feature = "yew-component")]
 	pub use crate::yew::{ApexChartComponent, ApexChartComponentProps};
-	#[cfg(feature = "leptos-component")]
+	#[cfg(feature = "leptos")]
 	pub use crate::leptos::ApexChartComponent;
 }
