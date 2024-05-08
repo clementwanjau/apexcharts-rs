8cfc43e50d6fe56d09ce7038f48b755466ed7da0
clement <clementwanjau@gmail.com>
2024-05-09 01:40:23 +0300

    Update the library to include rendering charts in leptos behind the `leptos` feature.

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,14 @@ mod bindings;
 
 #[cfg(feature = "yew-component")]
 mod yew;
+#[cfg(feature = "leptos-component")]
+mod leptos;
 
 pub mod prelude {
 	pub use crate::bindings::ApexChart;
 	pub use crate::options::{ChartType, ChartSeries, SeriesData, to_jsvalue};
 	#[cfg(feature = "yew-component")]
 	pub use crate::yew::{ApexChartComponent, ApexChartComponentProps};
+	#[cfg(feature = "leptos-component")]
+	pub use crate::leptos::ApexChartComponent;
 }
